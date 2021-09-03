4d98cdfa487791f6592f570f7a0fccc227075469
iyanuoluwa <iyanu@kenshomedia.com>
2021-09-03 22:09:34 +0100

    updated featured section

---
+++ src/components/Featured/Featured.styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledSection = styled.section`
+  padding: 3rem 0;
+`;
+
+export const StyledContainer = styled.div`
+  row-gap: 2rem;
+  text-transform: uppercase;
+`;
